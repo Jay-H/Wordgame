5c6582ddf7d0a63c3b43ab9bbcf3b9bc5dc2c260
christophereleven <christopherhaddad12@gmail.com>
2025-10-30 17:41:54 -0700

    Signed-off-by: christophereleven <christopherhaddad12@gmail.com>

--- run_headless.sh
+++ run_headless.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Navigate to the project directory
-cd /home/testbed/Documents/Wordgame
+cd /home/christopherhaddad12/my_game_server/Wordgame
 
 # Pull the latest changes
 git pull origin master
 
 # Export the Godot project
 echo "Exporting new Godot project..."
-godot --headless --export-release "Linux" build/server/wordsearch.x86_64
+./Godot_v4.4-stable_linux.x86_64 --headless --export-release "Linux" build/server/wordsearch.x86_64
 
 # Restart the systemd service to pick up the new executable
 echo "Restarting Godot server service..."
